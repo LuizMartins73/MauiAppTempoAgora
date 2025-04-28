bdce11a55019c83c7216095f54ff5be623445b70
231223 <231223@CPD03-M03>
2025-04-28 11:41:05 -0300

    Aula 28/04

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,24 +1,76 @@
-﻿namespace MauiAppTempoAgora
+﻿using Java.Util.Concurrent;
+using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if(!string.IsNullOrEmpty(txt_cidade))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null) 
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sumset} \n" +
+                                         $"Temp Máx: {t.temp_max}\n" +
+                                         $"Tempo_Min? {t.temp_min}\n" +
+
+                     lbl_res.Text = dados_previsao;
+
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                    $"type=map&location=coordinates&metricRain=default&metricTemp=default" +
+                    $"&metricWind=default&zoom=5&overlay=wind&product=&level=surface&lat={t.lat.ToString().Replace(",", ".")}&lon={t.lon.ToString().Replace(",", ".")}";
+
+                        wv_mapa.Source = mapa;
+
+                        Debug.WriteLine(mapa);
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem Dados de Previsão";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void Search_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest request = new GeolocationRequest
+               (
+                    GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10)
+               );
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+
+
+            }
+            }
         }
     }
 
